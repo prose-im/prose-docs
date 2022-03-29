6c90d7020c43c3147e1bda72725f3c17737354d2
Valerian Saliou <valerian@valeriansaliou.name>
2022-03-29 11:53:55 +0200

    Import example

---
+++ tools/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##
+#  Prose - Docs
+#  Build script
+#
+#  Copyright 2022, Prose Foundation
+#  Author: Valerian Saliou https://valeriansaliou.name/
+##
+
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+BASE_DIR="$ABSPATH/../"
+
+pushd "$BASE_DIR" > /dev/null
+  cur_env="development"
+
+  if [ ! -z "$1" ]; then
+    cur_env="$1"
+  fi
+
+  config_path_all="./config/common.json,./config/$cur_env.json"
+  config_path_local="./config/local.json"
+
+  if [ -f "$config_path_local" ]; then
+    config_path_all+=",$config_path_local"
+  fi
+
+  $(npm bin)/chappe build \
+    --env="$cur_env" \
+    --config="$config_path_all" \
+    --assets="./res/assets" \
+    --data="./src/data" \
+    --dist="./build"
+  rc=$?
+popd > /dev/null
+
+exit $rc
+
